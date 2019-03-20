9619f1ba720f50e17c515696924b653fb00ce71e
ca6.josequel <ca6.josequel@enfrausaph.com>
2019-03-21 05:04:57 +0800

    make it better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+	title = 'app';
+	myStyle: object = {};
+	myParams: object = {};
+	width: number = 100;
+	height: number = 100;
+  ngOnInit() {
+        this.myStyle = {
+            'position': 'fixed',
+            'width': '100%',
+            'height': '100%',
+            'z-index': -1,
+            'top': 0,
+            'left': 0,
+            'right': 0,
+            'bottom': 0,
+            'background-image': `linear-gradient(to bottom, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0.5) 100%), url(http://mourithemes.com/mason/mason/images/home/home-bg-2.jpeg)`
+        };
+ 
+    this.myParams = {
+        particles: {
+            number: {
+                value: 75,
+            },
+            color: {
+                value: '#fff'
+            },
+	        line_linked: {
+	          color: '#fff',
+	          opacity: 1
+	        },
+            shape: {
+                type: 'circle',
+            },
+            size: {
+            	value: '5'
+            },
+            opacity:{
+            	value: '0.5'
+            }
+        }
+
+    };
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ParticlesModule } from 'angular-particle';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
